2102270570bbcfc313135843b15992509f30c142
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-09-19 12:37:43 +0000

    fixed disabled edit button color opacity

--- client/src/ReservationList/ReservationList.tsx
+++ client/src/ReservationList/ReservationList.tsx
@@ -49,7 +49,7 @@ const ReservationList = () => {
                         </Link>
                       ) : (
                         <button
-                          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded opacity-20"
                           disabled
                         >
                           Edit
