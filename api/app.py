a2eec3ef79589692603df5f6832f8665cda2dba4
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-09-19 06:22:07 +0000

    implemented update reservation api

--- api/app.py
+++ api/app.py
@@ -13,3 +13,7 @@ def get_reservations():
 @app.post('/reservation')
 def create_reservation():
     return helpers.create_reservation(request.form)
+
+@app.put('/reservation/<int:id>')
+def update_reservation(id):
+    return helpers.update_reservation(id, request.form)
--- api/helpers.py
+++ api/helpers.py
@@ -45,3 +45,28 @@ def create_reservation(reservation_payload):
     # Commits the inserted data
     get_db().commit()
     return 'reservation created'
+
+# Updates a reservation
+def update_reservation(reservation_id, reservation_payload):
+    # Create a dictionary to hold the updated values
+    updated_values = {}
+    # Check and update each field if it exists in reservation_payload
+    if 'reservation_first_name' in reservation_payload:
+        updated_values['reservation_first_name'] = reservation_payload['reservation_first_name']
+    if 'reservation_last_name' in reservation_payload:
+        updated_values['reservation_last_name'] = reservation_payload['reservation_last_name']
+    if 'phone_number' in reservation_payload:
+        updated_values['phone_number'] = reservation_payload['phone_number']
+    if 'number_of_guests' in reservation_payload:
+        updated_values['number_of_guests'] = reservation_payload['number_of_guests']
+    # Build the SQL query dynamically based on the updated values
+    update_reservation_query = "UPDATE reservations SET "
+    update_reservation_query += ", ".join(f"{key} = ?" for key in updated_values)
+    update_reservation_query += " WHERE ID = ?"
+    # Prepare the arguments for the query
+    args = [updated_values[key] for key in updated_values] + [reservation_id]
+    # Execute the update query
+    query_db(update_reservation_query, args)
+    # Commits the updated data
+    get_db().commit()
+    return 'reservation updated'
